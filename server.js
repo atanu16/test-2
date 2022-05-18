108273e76bc7523cc5e0b155de66a7fcb9c070b2
Ayush Sinha <ayushsinhajsr20@gmail.com>
2022-05-18 20:49:28 +0530

    Setup server file

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
+require('dotenv').config()
 const express = require("express");
 const https =require("https");
 
-
 const app = express()
-app.use(express.static("public"))
-app.set('view engine','ejs')
-app.use(express.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const port = 3000;
+
+// Database connection
+const connection = require('./database.js');
 
 // Routes
 const routes = require('./routes/route')
 app.use(routes)
 
-app.get('/',(req,res)=>res.render('index'))
+// Server Listen
+connection.then(db => {
+    if(!db) return process.exit(1);
 
+    // listen to the http server
+    app.listen(port, () => {
+        console.log(`Server is running on port: http://localhost:${port}`)
+    })
 
-app.listen(3000, function()
-{
-  console.log("Server is running in port 3000");
+    app.on('error', err => console.log(`Failed To Connect with HTTP Server : ${err}`));
+    // error in mondb connection
+}).catch(error => {
+    console.log(`Connection Failed...! ${error}`);
 });
